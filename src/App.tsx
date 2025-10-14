6fe8718e6183301e91b5b57f43ff2b5e2b562bef
vebinua-genashtim <vincent@genashtim.com>
2025-10-14 09:06:53 +0800

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,27 +13,47 @@ import ISO9001 from './pages/ISO9001';
 import ContactUs from './pages/ContactUs';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [currentPage]);
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home setCurrentPage={setCurrentPage} />;
+      case 'about-us':
+        return <AboutUs />;
+      case 'certificate':
+        return <Certificate />;
+      case 'client-directory':
+        return <ClientDirectory />;
+      case 'assessment-regulation':
+        return <AssessmentRegulation />;
+      case 'initial-audit':
+        return <InitialAudit />;
+      case 'surveillance-audit':
+        return <SurveillanceAudit />;
+      case 'recertification-audit':
+        return <RecertificationAudit />;
+      case 'iso9001':
+        return <ISO9001 />;
+      case 'contact-us':
+        return <ContactUs />;
+      default:
+        return <Home setCurrentPage={setCurrentPage} />;
+    }
+  };
+
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/certificate" element={<Certificate />} />
-            <Route path="/client-directory" element={<ClientDirectory />} />
-            <Route path="/assessment-regulation" element={<AssessmentRegulation />} />
-            <Route path="/initial-audit" element={<InitialAudit />} />
-            <Route path="/surveillance-audit" element={<SurveillanceAudit />} />
-            <Route path="/recertification-audit" element={<RecertificationAudit />} />
-            <Route path="/iso9001" element={<ISO9001 />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <main className="flex-grow">
+        {renderPage()}
+      </main>
+      <Footer setCurrentPage={setCurrentPage} />
+    </div>
   );
 }
 
